2a0268fd37806c1c1565f593d958a984507bf800
Sergey Petrenko <spetrenko@me.com>
2016-02-29 23:45:59 +0300

    More comments, better names

--- daemon.js
+++ daemon.js
@@ -1,4 +1,30 @@
 #!/usr/bin/env node
+/*
+ ** Digital Ocean Dynamic DNS daemon **
+ * https://github.com/digitalhitler/digitalocean-dynamicdns-daemon
+ * 
+ * (с) Sergey Petrenko 2016
+ * spetrenko@me.com
+ * 
+ * This daemon made for dynamic updating of subdomain A-record in DNS-zone
+ * provided by DigitalOcean.com service.
+ * 
+ ** How to use **
+ * Please set all required values in config.default.ini, rename it to config.ini
+ * and just start this script. You can set it up as daemon or service in your operating system.
+ * 
+ ** Attention **
+ * This script can be a little bit buggy because it was made for my own purposes
+ * and has been published because of few my friends just want the same and they
+ * are too lazy to write it own. Use it on your own risk!
+ * 
+ ** Troubleshooting **
+ * Script generates updates.log file, go there if you have issues. You can set debug to true in
+ * config.ini to enable more detailed logging.
+ *
+ */
+
+'use strict';
 
 var fs = require('fs'),
     jetpack = require('fs-jetpack'),
@@ -27,9 +53,9 @@ var app = {
     
     // determining required level to log:
     if (self.config.logging.debug) {
-      self.logLevel = log.VERBOSE
+      self.logLevel = log.VERBOSE;
     } else {
-      self.logLevel = log.INFO
+      self.logLevel = log.INFO;
     }
    
     
